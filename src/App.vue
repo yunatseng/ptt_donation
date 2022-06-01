fea15bd986e84c997b9a9e6c80aeb7b553195ab0
yunaTseng <fav0124@gmail.com>
2022-06-01 17:09:23 +0800

    ADD: main page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,94 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+
+        <Main />
+   
+    </v-main>
+
+    <v-footer dark padless>
+      <v-card
+        flat
+        tile
+        color="primary"
+        class="white--text text-left"
+        width="100%"
+      >
+        <v-card-text class="white--text">
+          <!-- <ol>
+            <li v-for="(citation, i) in citeList" :key="i">
+              <a
+                :id="`event_${i}`"
+                :tabindex="i"
+                class="white--text footer-citation"
+                :href="citation.url"
+                target="_blank"
+              >
+                {{ citation.name }}
+              </a>
+            </li>
+          </ol> -->
+        </v-card-text>
+
+        <v-divider />
+
+        <v-card-text class="white--text text-center">
+          © {{ new Date().getFullYear() }} — Skylar
+          <a
+            href="mailto:fav0124@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            ><v-icon>mdi-email</v-icon></a
+          >
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Main from "./components/Main.vue";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    Main,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,102 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-img
+        class="white--text"
+        src="../assets/ptt_banner.jpg"
+        gradient="to top right, rgba(15,15,18,.5), rgba(25,25,30,.8)"
+        :lazy-src="`https://picsum.photos/10/6?image=1`"
+        :height="$vuetify.breakpoint.smAndDown?'100%':'400'"
+      >
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            />
+          </v-row>
+        </template>
+        <v-container class="fill-height">
+          <v-row
+            align="center"
+            :class="$vuetify.breakpoint.smAndDown?'pl-4 pr-6':''"
+          >
+          
+            <v-row justify="end">
+           
+              <div class="headline text-uppercase font-weight-regular ">
+                PTT 超商挖眼案 捐款紀錄
+              </div>
+                 <div class="font-weight-light ml-2">
+                Sep 26, 2021
+              </div>
+            </v-row>
+          </v-row>
+        </v-container>
+      </v-img>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Main',
+
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+  }
+</script>
