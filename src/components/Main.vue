283249ddc45494bbf3490801744c0091682ab77f
yunaTseng <fav0124@gmail.com>
2022-07-29 18:35:24 +0800

    ADD: real data

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -70,17 +70,17 @@
           <v-carousel-item v-for="(slide, i) in slides" :key="i">
             <v-sheet color="#d3c9cb3d" height="110%">
               <v-row
-                class="fill-height pa-6 pa-md-15"
+                class="fill-height pa-6 pa-md-15 justify-space-evenly justify-md-center"
                 :class="{'flex-column':$vuetify.breakpoint.smAndDown}"
                 style="flex-wrap: nowrap; "
-                justify="center"
+                
               >
-                <div class="text-h6" :class="{'carousel-text1':$vuetify.breakpoint.smAndDown}" style="width: 40%; text-align: center">
+                <div class="text-md-h6" :class="{'carousel-text1':$vuetify.breakpoint.smAndDown}" style="width: 40%; text-align: center">
                   作者：{{ slide.author }}<br />
                   {{ slide.date }}
                 </div>
                 <div
-                  class="text-subtitle-1"
+                  class="text-md-subtitle-1"
                   :class="{'carousel-text2':$vuetify.breakpoint.smAndDown}"
                   style="width: 52%; "
                 >
@@ -90,7 +90,8 @@
                     v-if="textImgUrl(slide.final_text).length > 0"
                   >
                     <img
-                      style="width: 40%; margin-top: 50px"
+                      style="width: 40%; margin-top: 50px;margin-left:10px"
+                      :class="{'carousel-img':$vuetify.breakpoint.smAndDown}"
                       v-for="(url, i) in textImgUrl(slide.final_text)"
                       :key="i"
                       :src="textImgUrl(slide.final_text)[i]"
@@ -119,21 +120,21 @@
           <ul>
             <li>
               <a
-                style="color: #231306"
+                class="related-links"
                 href="https://drive.google.com/file/d/103ddeuTXk4tA3R38unoXdotlxYCEWbCd/view"
                 >民間版《精神衛生法》說明及全文</a
               >
             </li>
             <li>
               <a
-                style="color: #231306"
+                class="related-links"
                 href="https://isbn.ncl.edu.tw/NEW_ISBNNet/main_DisplayRecord.php?&Pact=Display&Pstart=1"
                 >正常與瘋狂的天秤: 談精神疾病與司法鑑定</a
               >
             </li>
             <li>
               <a
-                style="color: #231306"
+                class="related-links"
                 href="https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=L0020030"
                 >現行精神衛生法條文</a
               >
@@ -145,21 +146,22 @@
           <ul>
             <li>
               <a
-                style="color: #231306"
+                class="related-links"
+               
                 href="https://www.cna.com.tw/news/firstnews/202111210025.aspx"
                 >超商店員疑勸戴口罩遭刺死 犯嫌遭羈押</a
               >
             </li>
             <li>
               <a
-                style="color: #231306"
+                class="related-links"
                 href="https://www.cna.com.tw/news/asoc/202110200372.aspx"
                 >未戴口罩被提醒惱羞毆打超商店員顧客 桃園男送辦</a
               >
             </li>
             <li>
               <a
-                style="color: #231306"
+                class="related-links"
                 href="https://www.cna.com.tw/news/asoc/202110270059.aspx"
                 >不滿被要求戴口罩 台中醉男毆打店員遭法辦</a
               >
@@ -177,6 +179,7 @@
 </template>
 
 <script>
+import PTT_DATA  from '../data/data';
 /**
  *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
  *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
@@ -197,36 +200,19 @@
 
 export default {
   name: "Main",
-
   data: () => ({
-    slides: [
-      {
-        author: "grace0523 (小小)",
-        date: "Tue Oct  5 12:50:27 2021",
-        final_text:
-          "會攻擊人的神經病真的很可惡！  是不是要找一個外島把神經病都關在那  另外雖然捐的不多  但祝小妹妹能早日康復！    https://i.imgur.com/CkEI42A.jpg  ※ 引述《lovea》之銘言 : 幹，屏東挖眼案這麼嚴重的事 : 你們這些小朋友還在那邊洗啥雜魚的恩怨情仇 : 有沒有搞錯????? : 還有花蓮大樓倒塌，絕對要深究責任 : 別被塔綠班給蒙混過去了 : 通通給我洗起來！  ",
-      },
-      {
-        author: "prettypaty (喵電感應)",
-        date: "Tue Oct  5 12:46:19 2021",
-        final_text:
-          "本小妹只是個研究生，一個月薪水有限 略盡棉薄之力，拋磚期待引來更多的玉  https://i.imgur.com/3IMElmG.jpg  昨天看到一篇文說 鄉民這樣的捐款也無助於改善思覺失調症患者的治療與救治，沒有什麼意義 但我們也只是老百姓，我們用選票選出這個政府 讓這個政府召集專家解決問題，這責任不該落在百姓身上吧 在我們能力可及的範圍，我們能做的就是用金錢和物質支持受害者了 祝福潘小姐早日康復  ",
-      },
-      {
-        author: "wwman (記憶拼圖)",
-        date: "Tue Oct  5 12:45:50 2021",
-        final_text:
-          " 推這篇 幹 最近幾天在吵/炒什麼 狗屁小事情  什麼龍跟k ptt用戶都三十幾歲了 這種小事  還要花幾百篇去討論 真的很無言  就兩個小咖 各自的立場 價值觀 信仰有差別  有必要討論誰是絕對的對錯 跟挖以前的事蹟?     我覺得社會安全網 要社工+配兩便衣警察支援   去跟精神暴力者去家訪或調查時 全程攝影 必要時可以開槍   然後要政府要開設 收治收容的隔絕醫療環境   收治收容 有達標 無法跟正常社會活動的暴力精神者   監獄中度輕度犯人    可以在裡面工作比如 收容院的便利商店店員   或者請犯人在收容院裡面當職工 以獲得減刑   讓暴力精神者 有中度或輕度自由 但不能出去   社會社區去做可能犯罪 要有絕對高程度隔絕  的收治   讓精神暴力者在裡面 有隔絕的小型社區 跟小型環境   監獄犯人可以申請當員工   來獲得減刑  收治社區可參考外國 可以養寵物狗貓 等等各種可以調適身心的實驗作法   外國的對監獄或者精神者有很多實驗作法 (這邊不是很確定 但好像有些是以前電視   或網路上新聞看過的做法)   比如 養寵物   或者       輕音樂治療 或者 香精+寧靜藥物治療   或者可以訓練互動的團體遊戲 等等   請後面高手再推文補上吧   以上是我對社會安全網     簡略的建議      請大家可以補強跟討論~~   排版有點亂  請原諒 謝謝大家~  ",
-      },
-      {
-        author: "shaoyanyan (小鹽)",
-        date: "Tue Oct  5 12:42:45 2021",
-        final_text:
-          "https://i.imgur.com/sJP9k2T.jpg  捐款給她捐起來啦！ 幹你娘塔綠班去死！ 有症腐會做事! 相信症腐相信黨（個屁）！  ",
-      },
-    ],
+   
   }),
+  computed:{
+    slides(){
+      return this.shuffleArray(PTT_DATA).slice(0,14)
+    }
+  },
   methods: {
+    shuffleArray(inputArray){
+      inputArray.sort(()=> Math.random() - 0.5);
+      return inputArray
+    },
     textWithoutUrl(text) {
       const textArr = text.split(" ");
       const plainText = textArr.filter(
@@ -249,10 +235,21 @@ a {
 .carousel-text1{
   font-size: 18px !important;
   width: 100% !important;
+  font-weight: bold;
+  height: 60px;
 }
 .carousel-text2{
   margin-top: 15px;
   width: 100% !important;
   font-size: 16px !important;
+}
+.carousel-img{
+  width: 60% !important;
+  margin-top: 30px !important;
+  margin-left:0 !important
+}
+.related-links{
+  font-style: 14px;
+  color: #231306 !important
 }
 </style>
